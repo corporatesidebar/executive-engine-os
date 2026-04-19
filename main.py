a96797dfe685fc4babcb55240bb461a968c243e2
corporatesidebar <33066032+corporatesidebar@users.noreply.github.com>
2026-04-19 04:30:02 -0400

    Restrict CORS to specific origins

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://willwebb.ca",
+        "https://www.willwebb.ca"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
